5cbb61e71bba6485be0d811276139dcd3dccfbe9
Jingming Yang <yangjingming0621@gmail.com>
2025-11-08 22:07:43 -0800

    Initial commit

---
+++ Backend/constants/constants.go
@@ -0,0 +1,11 @@
+package constants
+
+const (
+    USER_INDEX = "user"
+    POST_INDEX = "post"
+    ES_URL = "http://10.168.0.2:9200"
+    ES_USERNAME = "Jimmy"
+    ES_PASSWORD = "134679abc1"
+    GCS_BUCKET = "jimmy-bucket-08"
+
+)
