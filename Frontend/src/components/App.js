b0cc7d85d1f97bcc5acbf604c7038d761be4d64c
Jingming Yang <yangjingming0621@gmail.com>
2025-11-08 22:12:47 -0800

    Initial commit

---
+++ Frontend/src/components/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import ResponsiveAppBar from "./ResponsiveAppBar";
+import Main from "./Main";
+
+import { TOKEN_KEY } from "../constants";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem(TOKEN_KEY) ? true : false
+    //we only check if it exists or not (not validation)
+  );
+
+  const logout = () => {
+    localStorage.removeItem(TOKEN_KEY);
+    setIsLoggedIn(false);
+  };
+
+  const loggedIn = (token) => {
+    if (token) {
+      localStorage.setItem(TOKEN_KEY, token);
+      setIsLoggedIn(true);
+    }
+  };
+
+  return (
+    <div className="App">
+      <ResponsiveAppBar isLoggedIn={isLoggedIn} handleLogout={logout} />
+      <Main isLoggedIn={isLoggedIn} handleLoggedIn={loggedIn} />
+    </div>
+  );
+}
+
+export default App;
